ba70ec01514781d217fb4c904869b4afae9f34a7
Jonno Schmidt <jonno@schmidtfam.us>
2014-11-13 18:31:34 -0500

    Made app build.

--- src/client/app/app.js
+++ src/client/app/app.js
@@ -5,7 +5,7 @@ angular.module('sqlvizApp', [
   'ngResource',
   'ngSanitize',
   'ui.router',
-  'ui.bootstrap',
+  'ui.bootstrap',grunt buildcontrol:heroku
   'ui.codemirror'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
