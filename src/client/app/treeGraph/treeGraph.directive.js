2f88283efc7c4964e618852166e12d3c546b50ab
Jonno Schmidt <jonno@schmidtfam.us>
2014-11-21 01:33:01 -0500

    Added tree graph directive

---
+++ src/client/app/treeGraph/treeGraph.directive.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('sqlvizApp', ['d3'])
+  .directive('treeGraph', ['d3Service', function (d3Service) {
+    return {
+      restrict: 'EA',
+      link: function (scope, element, attrs) {
+        d3Service.d3().then(function(d3) {
+          alert('hello');
+        });
+      }
+    };
+}]);
