58ab07ea6849d1fa2bcd4b9b51f3ccdf5eca1ed4
Jonno Schmidt <jonno@schmidtfam.us>
2014-11-21 00:40:32 -0500

    json for server

--- src/server/app.js
+++ src/server/app.js
@@ -7,10 +7,19 @@
 // Set default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+
+
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var config = require('./config/environment');
 // Setup server
 var app = express();
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+
 var server = require('http').createServer(app);
 require('./config/express')(app);
 require('./routes')(app);
