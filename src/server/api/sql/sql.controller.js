69e28f6b75ce4cf0abf472d09f937ff49a01e56c
Jonno Schmidt <jonno@schmidtfam.us>
2014-11-30 14:30:44 -0500

    changed sql on server side to prune tree and make it cleaner

--- src/server/api/sql/sql.controller.js
+++ src/server/api/sql/sql.controller.js
@@ -8,10 +8,87 @@ exports.index = function(req, res) {
   res.json([]);
 };
 
+var prune = function(data) {
+  if (!data) {
+    return null;
+  }
+  // if there is no name tag, then it is not anything useful
+  if (!data.name) {
+    // discard
+    // never has children
+    return null;
+  }
+  else {
+    // don't care about whitespace
+    var text = data.name;
+    if ((text.indexOf('whitespace') > -1) || (text.indexOf('semicolon') > -1)) {
+      // discard
+      // never has children
+    }
+    else {
+      if ((data.name.indexOf('start') > -1) || (data.name.indexOf('source') > -1)){
+        data = data.children[0];
+      }
+
+      if (data.range) {
+        data.statement = data.source.substr(data.range.location, data.range.length);
+      }
+
+      if (data.children) {
+        var kids = [];
+        for (var i = 0; i < data.children.length; i++) {
+          var t = prune(data.children[i]);
+          if (t) {
+            kids.push(t);
+          }
+        }
+        data.children = kids;
+      }
+      else {
+        console.log(data);
+      }
+      return data;
+    }
+  }
+  return null;
+};
+
+/**
+ * Understand what the SQL is doing.
+ * Find out what tables there are in the statement and
+ * what columns are in each table.
+ *
+ * @param data
+ */
+var interpretSQL = function(data) {
+  var tables = [];
+
+  var queue = [];
+  queue.push(data);
+
+  while (queue.length > 0) {
+    var cur = queue.pop();
+    console.log(cur.name);
+    if (cur.name == 'table_name') {
+      tables[cur.name] = 'lol';
+    }
+    for (var i = 0; i < cur.children.length; i++) {
+      queue.push(cur.children[i]);
+    }
+  }
+
+  console.log(tables);
+};
+
+
 exports.parseSQL = function(req, res) {
   var command = req.body.sql;
   if (command) {
     var tree = sql.parse(command);
+    //tree = prune(tree);
+
+    //interpretSQL(tree);
+
     return res.json(tree);
   }
   else {
