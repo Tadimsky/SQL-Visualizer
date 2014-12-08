45d30db742d4ff3c6769e30ae953f2ce5a96f6d2
Jonno Schmidt <jonno@schmidtfam.us>
2014-12-07 22:50:13 -0500

    delayed request

--- src/client/app/main/main.controller.js
+++ src/client/app/main/main.controller.js
@@ -7,20 +7,26 @@ angular.module('sqlvizApp')
     $scope.parsedTreeText = '';
     $scope.parsedTree;
 
+    var timeoutToken = -1;
     $scope.$watch('sqlCommand',
       function(newValue, oldValue) {
         if ( newValue !== oldValue ) {
           if (newValue.indexOf(';') != newValue.length - 1) {
             newValue = newValue + ';';
           }
-          $http.post('/api/sql', {sql: newValue})
-            .success(function(data, status, headers) {
-              $scope.parsedTree = data; //cleanupTree(data);
-              $scope.parsedTreeText = JSON.stringify(data, undefined, 4);
-            })
-            .error(function(data, status) {
-              alert(status);
-            });
+          if (timeoutToken) {
+            clearTimeout(timeoutToken);
+          }
+          timeoutToken = setTimeout(function() {
+            $http.post('/api/sql', {sql: newValue})
+              .success(function(data, status, headers) {
+                $scope.parsedTree = data; //cleanupTree(data);
+                $scope.parsedTreeText = JSON.stringify(data, undefined, 4);
+              })
+              .error(function(data, status) {
+                alert(status);
+              });
+          }, 150);
         }
       }
     );
